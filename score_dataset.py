b8ff50eccd3ea89195c11b31cf635550d8f91653
bilalord <93769447+bilalord@users.noreply.github.com>
2021-11-25 00:23:48 +0100

    Create score_dataset.py

---
+++ score_dataset.py
@@ -0,0 +1,20 @@
+# Not mine, from KAGGLE.COM!
+# https://www.kaggle.com/ryanholbrook/feature-engineering-for-house-prices
+# RYAN HOLBROOK & ALEXIS COOK
+
+def score_dataset(X, y, model=XGBRegressor()):
+    # Label encoding for categoricals
+    #
+    # Label encoding is good for XGBoost and RandomForest, but one-hot
+    # would be better for models like Lasso or Ridge. The `cat.codes`
+    # attribute holds the category levels.
+    for colname in X.select_dtypes(["category"]):
+        X[colname] = X[colname].cat.codes
+    # Metric for Housing competition is RMSLE (Root Mean Squared Log Error)
+    log_y = np.log(y)
+    score = cross_val_score(
+        model, X, log_y, cv=5, scoring="neg_mean_squared_error",
+    )
+    score = -1 * score.mean()
+    score = np.sqrt(score)
+    return score
